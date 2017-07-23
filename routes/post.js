81a650caba557fc598d27dbb1fab17fea42a2e9c
Saurabh Kumar <saurabh@Saurabh-Kumar241.local>
2017-07-23 14:57:30 +0530

    add/edit

--- routes/post.js
+++ routes/post.js
@@ -4,13 +4,42 @@ var Post = require('../schema/post');
 
 /* GET post page. */
 router.get('/', function(req, res, next) {
-  res.render('post', { title: 'Post' });
+  Post.find({}, function(err, posts) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('post', { title: 'Post', posts: posts });
+    }
+  }); 
+  
+});
+
+
+/* GET SINGLE POST BY ID */
+router.get('/edit/:id', function(req, res, next) {
+  Post.findById(req.params.id, function (err, post) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('post', { title: 'Post Details', postDetail: post });
+    }
+  });
+});
+
+/* DELETE POST BY ID */
+router.get('/destroy/:id', function(req, res, next) {
+  Post.findByIdAndRemove(req.params.id, function (err, post) {
+    if (err) {
+      res.render('post', { title: 'Hey', message: 'Not deleted!' });
+    } else {
+      res.render('post', { title: 'Hey', message: 'Post deleted!' });
+    }
+  });
 });
 
 
 /* POST Data. */
 router.post('/', function(req, res, next) {
- console.log(req.body);
  var data = new Post(req.body);
  data.save(function(err){
   if(err){
@@ -19,6 +48,18 @@ router.post('/', function(req, res, next) {
     res.render('post', { title: 'Hey', message: 'Request received!' });
   }
  })
+});
+
+
+/* UPDATE POST */
+router.put('/post/:id', function(req, res, next) {
+  Product.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if(err){
+    res.render('post', { title: 'Hey', message: 'Invalid request!' });
+  } else {
+    res.render('post', { title: 'Hey', message: 'Post updated!' });
+  }
+  });
 });
 
 module.exports = router;
